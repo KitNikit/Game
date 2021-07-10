39a48c11541f7fac740b899e1748cfc43c3b43b9
KitNikit <vclan@mail.ru>
2021-07-10 16:45:23 +0300

    init

--- js/game.js
+++ js/game.js
@@ -64,7 +64,11 @@ function draw() {
 					yPos + bird.height >= pipe[i].y + pipeUp.height + gap)) ||
 			yPos + bird.height >= cvs.height - fg.height
 		) {
-			location.reload();
+			// document.location.reload();
+
+			score = 0;
+			xPos = 10;
+			yPos = 150;
 		}
 		if (pipe[i].x == 5) {
 			score++;
